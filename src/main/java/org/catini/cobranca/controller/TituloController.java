dbfa560b4c02001184f1d2da7113ab8dec0f569e
Guilherme Catini <guilherme.catini@iv2.com.br>
2017-05-09 16:07:39 -0300

    Aula 06 Finalizada

--- src/main/java/org/catini/cobranca/controller/TituloController.java
+++ src/main/java/org/catini/cobranca/controller/TituloController.java
@@ -1,9 +1,14 @@
 package org.catini.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.catini.cobranca.model.StatusTitulo;
 import org.catini.cobranca.model.Titulo;
 import org.catini.cobranca.repository.Titulos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,15 +21,22 @@ public class TituloController {
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
 	public ModelAndView salvar(Titulo titulo) {
 		titulos.save(titulo);
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem", "Título salvo amigão!");
 		return mv;
 	}
+	
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
+	}
+	
 }
