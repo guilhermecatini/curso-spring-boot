da4f666d2d5b3d64ee5779084972b9fbfefab83f
Guilherme Catini <guilherme.catini@iv2.com.br>
2017-05-09 14:01:37 -0300

    Aula 03 Finalizada

--- src/main/java/org/catini/cobranca/controller/TituloController.java
+++ src/main/java/org/catini/cobranca/controller/TituloController.java
@@ -1,12 +1,22 @@
 package org.catini.cobranca.controller;
 
+import org.catini.cobranca.model.Titulo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo() {
+		return "CadastroTitulo";
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		// TODO: Salvar no banco de dados
+		System.out.println(">>>> " + titulo.getDescricao());
 		return "CadastroTitulo";
 	}
 }
---
+++ src/main/java/org/catini/cobranca/model/StatusTitulo.java
@@ -0,0 +1,16 @@
+package org.catini.cobranca.model;
+
+public enum StatusTitulo {
+	PENDENTE("Pendente"),
+	RECEBIDO("Recebido");
+	
+	private String descricao;
+	
+	StatusTitulo(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao(){
+		return this.descricao;
+	}
+}
