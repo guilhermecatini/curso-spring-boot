c231d74584e9e9ba50a411ea54ca504ce009e0c6
Guilherme Catini <guilherme.catini@iv2.com.br>
2017-05-09 11:57:09 -0300

    My First Commit

---
+++ src/main/java/org/catini/cobranca/controller/TituloController.java
@@ -0,0 +1,12 @@
+package org.catini.cobranca.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TituloController {
+	@RequestMapping("/titulos/novo")
+	public String novo() {
+		return "CadastroTitulo";
+	}
+}
